83df6b55852b594a6e02c893ac3bb7d2be2c7f4b
KJeffree <k.jeffree@gmail.com>
2020-02-04 16:50:03 +0000

    shapes can rotate and still stay within boundaries

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    public bool canMove = true;
-    public int widthFromPivot;
+    [SerializeField] int[] widthsFromPivot;
+    [SerializeField] int[] widthsBeforePivot;
+
+    int rotation = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,22 +17,19 @@ public class Shape : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) && canMove){
-            canMove = false;
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)){
+            float xDifference = Input.GetKeyDown(KeyCode.RightArrow) ? 0.5f : -0.5f;
             float currentXPosition = transform.position.x;
-            float newXPosition = Mathf.Clamp(currentXPosition + 0.5f, 0.5f, 5f - ((float)(widthFromPivot - 1) * 0.5f));
+            float newXPosition = Mathf.Clamp(currentXPosition + xDifference, 0.5f + ((float)(widthsBeforePivot[rotation]) * 0.5f), 5f - ((float)(widthsFromPivot[rotation] - 1) * 0.5f));
             Vector3 newPosition = new Vector3(newXPosition, transform.position.y, transform.position.z);
             transform.position = newPosition;
-            canMove = true;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && canMove){
-            canMove = false;
-            float currentXPosition = transform.position.x;
-            float newXPosition = Mathf.Clamp(currentXPosition - 0.5f, 0.5f, 5f - ((float)(widthFromPivot - 1) * 0.5f));
-            Vector3 newPosition = new Vector3(newXPosition, transform.position.y, transform.position.z);
-            transform.position = newPosition;
-            canMove = true;
+
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Vector3 originalRotation = transform.rotation.eulerAngles;
+            originalRotation.z -= 90;
+            transform.rotation = Quaternion.Euler(originalRotation);
+            rotation = rotation < 3 ? rotation + 1 : 0;
         }
-       
     }
 }
