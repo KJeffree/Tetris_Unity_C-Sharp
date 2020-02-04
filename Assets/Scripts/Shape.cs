d0de02d09865010479298a834c7ad03e5a51af24
KJeffree <k.jeffree@gmail.com>
2020-02-04 14:59:40 +0000

    added movement left and right to shapes

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public bool canMove = true;
+    public int widthFromPivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && canMove){
+            canMove = false;
+            float currentXPosition = transform.position.x;
+            float newXPosition = Mathf.Clamp(currentXPosition + 0.5f, 0.5f, 5f - ((float)(widthFromPivot - 1) * 0.5f));
+            Vector3 newPosition = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            transform.position = newPosition;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && canMove){
+            canMove = false;
+            float currentXPosition = transform.position.x;
+            float newXPosition = Mathf.Clamp(currentXPosition - 0.5f, 0.5f, 5f - ((float)(widthFromPivot - 1) * 0.5f));
+            Vector3 newPosition = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            transform.position = newPosition;
+            canMove = true;
+        }
+       
+    }
+}
